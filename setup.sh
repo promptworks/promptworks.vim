c1de10750e6b6b81e02bd61a24e0458069c74d68
Mike Nicholaides <mike.nicholaides@gmail.com>
2015-04-19 14:47:00 -0400

    Improve setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-set -e
+this_repo=https://github.com/promptworks/promptworks.vim.git
 
 # Backup .vim directory
-[[ -d ~/.vim && (! -L ~/.vim) ]] && mv ~/.vim ~/.vim.bak
 
-# Clone PromptWorks vim config to be the .vim directory
-git clone https://github.com/promptworks/promptworks.vim.git ~/.vim
+cd ~
 
-#Backup .vimrc
+if [[ -d ~/.vim ]]
+then
+  # If it's this repo
+  if (cd ~/.vim && git remote -v | grep "$this_repo")
+  then
+    (cd ~/.vim && git pull)
+  else
+    mv ~/.vim ~/.vim.bak
+  fi
+else
+  # Clone PromptWorks vim config to be the .vim directory
+  git clone $this_repo ~/.vim
+fi
+
+set -e
+
+
+# Backup .vimrc unless it's a symlink
 [[ -f ~/.vimrc && (! -L ~/.vimrc) ]] && mv ~/.vimrc ~/.vimrc.bak
 
 # Link our vimrc to ~/.vimrc
 ln -sF ~/.vim/vimrc ~/.vimrc
 
 # Install vundle plugin
-git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+if [[ ! -d ~/.vim/bundle/vundle ]]
+then
+  git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
 
-# Install all other plugins using Vundle
+# Install/Update all other plugins using Vundle
 vim -u $HOME/.vim/vimrc.bundles +PluginUpdate +qall
